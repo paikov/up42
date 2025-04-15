f80a5d2fa57496539ab0dd15197b0f153f27ae3e
Daniel Paikov <paikov@gmail.com>
2025-04-15 13:58:31 +0200

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,6 +4,12 @@ variable "ns" {
   default     = "up42-ns"
 }
 
+variable "service_name" {
+  description = "Default s3www service name to be used if not configured in Helm"
+  type        = string
+  default     = "s3www-service"
+}
+
 variable "helm_chart_path" {
   description = "Path to Helm chart directory"
   type        = string
@@ -14,10 +20,4 @@ variable "release_name" {
   description = "Name of Helm release"
   type        = string
   default     = "up42-release"
-}
-
-variable "service_name" {
-  description = "Name of s3www service"
-  type        = string
-  default     = "s3www-service"
 }
